92082055885de9e370777ce16c274edfffc7dfce
surendra <suredon456@gmail.com>
2023-12-06 12:40:16 +0530

    ui packages

--- app/ui/IssuesTable.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {fetchAllIssues} from '@/app/lib/data'
-
- export default async function IssuesTable (){
-   const ListOfIssues = await  fetchAllIssues()
-   console.log(ListOfIssues)
-  return (
-    <div>Totalissues</div>
-  )}
