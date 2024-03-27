f62dbe0510e136b50e286bb2841a47f2b971ad71
surendra <suredon456@gmail.com>
2024-03-27 20:48:44 +0530

    added edit button

--- Trace.com/apps/web/app/dashboard/issues/[id]/page.tsx
+++ Trace.com/apps/web/app/dashboard/issues/[id]/page.tsx
@@ -1,8 +1,11 @@
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
 import prisma from "@repo/database";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
-import IssueStatusBadge from "../../../components/IssueStatusBadge";
+import { IssueStatusBadge } from "../../../components";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import Link from "next/link";
+
 interface Props {
   params: { id: string };
 }
@@ -15,17 +18,209 @@ const IssueDetailPage = async ({ params }: Props) => {
   if (!issue) return notFound();
 
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex className="space-x-3" my="2">
-        <IssueStatusBadge status={issue.status} />
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex className="space-x-3" my="2">
+          <IssueStatusBadge status={issue.status} />
+          <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt="4">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={``} />
+          Edit Issue
+        </Button>
+      </Box>
+    </Grid>
   );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
 };
 
 export default IssueDetailPage;
