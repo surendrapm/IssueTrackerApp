348fa06f5ce6c45a11bd023f58d3bcec076a85f9
surendra <suredon456@gmail.com>
2024-04-17 20:25:37 +0530

    added a delete button

--- Trace.com/apps/web/app/dashboard/layout.tsx
+++ Trace.com/apps/web/app/dashboard/layout.tsx
@@ -1,17 +1,20 @@
-import { Theme } from '@radix-ui/themes'
+import { Container, Theme } from '@radix-ui/themes'
 import React from 'react'
 import Navbar from '../ui/Navbar'
 import '@radix-ui/themes/styles.css';
 
+
 const layout = ({children}:{children:React.ReactNode}) => {
   return (
     <div>
-         <Theme accentColor="violet">
-          <Navbar/>
-        <div className='p-5'>{children}</div> 
-        </Theme>
+      <Theme accentColor="violet">
+        <Navbar />
+        <main className="p-5">
+          <Container>{children}</Container>
+        </main>
+      </Theme>
     </div>
-  )
+  );
 }
 
 export default layout
